508bad7347142a551019e255ef56a7d92d885040
Duncan Macleod <duncan.macleod@ligo.org>
2017-07-13 12:48:25 +0100

    profile.d: added ligo-hosts aliases profile

---
+++ etc/profile.d/ligo-hosts.sh
@@ -0,0 +1,173 @@
+#
+# Profile aliases for LIGO Data Grid hosts
+#
+# Author: Duncan Macleod <duncan.macleod@ligo.org>
+#
+
+# Caltech
+export CIT=ldas-grid.ligo.caltech.edu
+alias cit="echo $ gsissh ${CIT}; gsissh ${CIT}"
+export CITPCDEV1=ldas-pcdev1.ligo.caltech.edu
+alias citpcdev1="echo $ gsissh ${CITPCDEV1}; gsissh ${CITPCDEV1}"
+export CITPCDEV2=ldas-pcdev2.ligo.caltech.edu
+alias citpcdev2="echo $ gsissh ${CITPCDEV2}; gsissh ${CITPCDEV2}"
+export CITPCDEV3=ldas-pcdev3.ligo.caltech.edu
+alias citpcdev3="echo $ gsissh ${CITPCDEV3}; gsissh ${CITPCDEV3}"
+export CITPCDEV4=ldas-pcdev4.ligo.caltech.edu
+alias citpcdev4="echo $ gsissh ${CITPCDEV4}; gsissh ${CITPCDEV4}"
+export CITPCDEV5=ldas-pcdev5.ligo.caltech.edu
+alias citpcdev5="echo $ gsissh ${CITPCDEV5}; gsissh ${CITPCDEV5}"
+export CITPCDEV6=ldas-pcdev6.ligo.caltech.edu
+alias citpcdev6="echo $ gsissh ${CITPCDEV6}; gsissh ${CITPCDEV6}"
+
+
+# LIGO Hanford
+export LHO=ldas-grid.ligo-wa.caltech.edu
+alias lho="echo $ gsissh ${LHO}; gsissh ${LHO}"
+export LHOPCDEV1=ldas-pcdev1.ligo-wa.caltech.edu
+alias lhopcdev1="echo $ gsissh ${LHOPCDEV1}; gsissh ${LHOPCDEV1}"
+export LHOPCDEV2=ldas-pcdev2.ligo-wa.caltech.edu
+alias lhopcdev2="echo $ gsissh ${LHOPCDEV2}; gsissh ${LHOPCDEV2}"
+
+
+# LIGO Livingston
+export LLO=ldas-grid.ligo-la.caltech.edu
+alias llo="echo $ gsissh ${LLO}; gsissh ${LLO}"
+export LLOPCDEV1=ldas-pcdev1.ligo-la.caltech.edu
+alias llopcdev1="echo $ gsissh ${LLOPCDEV1}; gsissh ${LLOPCDEV1}"
+export LLOPCDEV2=ldas-pcdev2.ligo-la.caltech.edu
+alias llopcdev2="echo $ gsissh ${LLOPCDEV2}; gsissh ${LLOPCDEV2}"
+
+
+# MIT
+export MIT=ldas-grid.mit.edu
+alias mit="echo $ gsissh ${MIT}; gsissh ${MIT}"
+export MITPCDEV1=ldas-pcdev1.mit.edu
+alias mitpcdev1="echo $ gsissh ${MITPCDEV1}; gsissh ${MITPCDEV1}"
+export MITPCDEV2=ldas-pcdev2.mit.edu
+alias mitpcdev2="echo $ gsissh ${MITPCDEV2}; gsissh ${MITPCDEV2}"
+
+# atlas (AEI-Hannover)
+export ATLAS1=atlas1.atlas.aei.uni-hannover.de
+alias atlas1="echo $ gsissh ${ATLAS1}; gsissh ${ATLAS1}"
+export ATLAS2=atlas2.atlas.aei.uni-hannover.de
+alias atlas2="echo $ gsissh ${ATLAS2}; gsissh ${ATLAS2}"
+export ATLAS3=atlas3.atlas.aei.uni-hannover.de
+alias atlas3="echo $ gsissh ${ATLAS3}; gsissh ${ATLAS3}"
+export ATLAS4=atlas4.atlas.aei.uni-hannover.de
+alias atlas4="echo $ gsissh ${ATLAS4}; gsissh ${ATLAS4}"
+export ATLAS5=atlas5.atlas.aei.uni-hannover.de
+alias atlas5="echo $ gsissh ${ATLAS5}; gsissh ${ATLAS5}"
+export ATLAS6=atlas6.atlas.aei.uni-hannover.de
+alias atlas6="echo $ gsissh ${ATLAS6}; gsissh ${ATLAS6}"
+export ATLAS7=atlas7.atlas.aei.uni-hannover.de
+alias atlas7="echo $ gsissh ${ATLAS7}; gsissh ${ATLAS7}"
+export ATLAS8=atlas8.atlas.aei.uni-hannover.de
+alias atlas8="echo $ gsissh ${ATLAS8}; gsissh ${ATLAS8}"
+export TITAN1=titan1.atlas.aei.uni-hannover.de
+export TITAN1=titan1.atlas.aei.uni-hannover.de
+export TITAN1=titan1.atlas.aei.uni-hannover.de
+export TITAN1=titan1.atlas.aei.uni-hannover.de
+export TITAN1=titan1.atlas.aei.uni-hannover.de
+alias titan1="echo $ gsissh ${TITAN1}; gsissh ${TITAN1}"
+export TITAN2=titan2.atlas.aei.uni-hannover.de
+alias titan2="echo $ gsissh ${TITAN2}; gsissh ${TITAN2}"
+export TITAN3=titan3.atlas.aei.uni-hannover.de
+alias titan3="echo $ gsissh ${TITAN3}; gsissh ${TITAN3}"
+
+
+# SUGAR (Syracuse)
+export SUGAR=sugar.phy.syr.edu
+alias sugar="echo $ gsissh ${SUGAR}; gsissh ${SUGAR}"
+export SUGARPCDEV1=sugar-dev1.phy.syr.edu
+alias sugarpcdev1="echo $ gsissh ${SUGARPCDEV1}; gsissh ${SUGARPCDEV1}"
+export SUGARPCDEV2=sugar-dev2.phy.syr.edu
+alias sugarpcdev2="echo $ gsissh ${SUGARPCDEV2}; gsissh ${SUGARPCDEV2}"
+
+
+# Nemo (UWM)
+export HYDRA=hydra.phys.uwm.edu
+alias hydra="echo $ gsissh ${HYDRA}; gsissh ${HYDRA}"
+export HYDRAPCDEV1=pcdev1.phys.uwm.edu
+alias hydrapcdev1="echo $ gsissh ${HYDRAPCDEV1}; gsissh ${HYDRAPCDEV1}"
+export HYDRAPCDEV2=pcdev2.phys.uwm.edu
+alias hydrapcdev2="echo $ gsissh ${HYDRAPCDEV2}; gsissh ${HYDRAPCDEV2}"
+export MARLIN=marlin.phys.uwm.edu
+alias marlin="echo $ gsissh ${MARLIN}; gsissh ${MARLIN}"
+
+
+# Raven (ARCCA)
+export GEO2=geo2.arcca.cf.ac.uk
+alias geo2="echo $ gsissh spxdmm@${GEO2}; gsissh spxdmm@${GEO2}"
+export RAVEN=ravenlogin.arcca.cf.ac.uk
+alias raven="echo $ ssh spxdmm@${RAVEN}; ssh spxdmm@${RAVEN}"
+export RAVEN1=ravenlogin01.arcca.cf.ac.uk
+alias raven1="echo $ ssh spxdmm@${RAVEN1}; ssh spxdmm@${RAVEN1}"
+export RAVEN2=ravenlogin02.arcca.cf.ac.uk
+alias raven2="echo $ ssh spxdmm@${RAVEN2}; ssh spxdmm@${RAVEN2}"
+
+
+# IUCAA
+export IUCAA="ldas-grid.gw.iucaa.in"
+alias iucaa="echo $ gsissh ${IUCAA}; gsissh ${IUCAA}"
+export IUCAAPCDEV1=ldas-pcdev1.gw.iucaa.in
+alias iucaapcdev1="echo $ gsissh ${IUCAAPCDEV1}; gsissh ${IUCAAPCDEV1}"
+
+# alias cluster logins for shared accounts
+alias citdetchar="echo $ gsissh detchar@${CIT}; gsissh detchar@${CIT}"
+alias citcbc="echo $ gsissh cbc@${CIT}; gsissh cbc@${CIT}"
+alias citpcdev1detchar="echo $ gsissh detchar@${CITPCDEV1}; gsissh detchar@${CITPCDEV1}"
+alias citpcdev2detchar="echo $ gsissh detchar@${CITPCDEV2}; gsissh detchar@${CITPCDEV2}"
+alias citpcdev3detchar="echo $ gsissh detchar@${CITPCDEV3}; gsissh detchar@${CITPCDEV3}"
+alias citpcdev4detchar="echo $ gsissh detchar@${CITPCDEV4}; gsissh detchar@${CITPCDEV4}"
+alias citpcdev5detchar="echo $ gsissh detchar@${CITPCDEV5}; gsissh detchar@${CITPCDEV5}"
+alias citpcdev6detchar="echo $ gsissh detchar@${CITPCDEV6}; gsissh detchar@${CITPCDEV6}"
+alias citpcdev1cbc="echo $ gsissh cbc@${CITPCDEV1}; gsissh cbc@${CITPCDEV1}"
+alias citpcdev2cbc="echo $ gsissh cbc@${CITPCDEV2}; gsissh cbc@${CITPCDEV2}"
+alias citpcdev3cbc="echo $ gsissh cbc@${CITPCDEV3}; gsissh cbc@${CITPCDEV3}"
+alias citpcdev4cbc="echo $ gsissh cbc@${CITPCDEV4}; gsissh cbc@${CITPCDEV4}"
+alias citpcdev5cbc="echo $ gsissh cbc@${CITPCDEV5}; gsissh cbc@${CITPCDEV5}"
+alias citpcdev6cbc="echo $ gsissh cbc@${CITPCDEV6}; gsissh cbc@${CITPCDEV6}"
+alias lhodetchar="echo $ gsissh detchar@${LHO}; gsissh detchar@${LHO}"
+alias lhocbc="echo $ gsissh cbc@${LHO}; gsissh cbc@${LHO}"
+alias lhopcdev1detchar="echo $ gsissh detchar@${LHOPCDEV1}; gsissh detchar@${LHOPCDEV1}"
+alias lhopcdev2detchar="echo $ gsissh detchar@${LHOPCDEV2}; gsissh detchar@${LHOPCDEV2}"
+alias lhopcdev1cbc="echo $ gsissh cbc@${LHOPCDEV1}; gsissh cbc@${LHOPCDEV1}"
+alias llodetchar="echo $ gsissh detchar@${LLO}; gsissh detchar@${LLO}"
+alias llocbc="echo $ gsissh cbc@${LLO}; gsissh cbc@${LLO}"
+alias llopcdev1detchar="echo $ gsissh detchar@${LLOPCDEV1}; gsissh detchar@${LLOPCDEV1}"
+alias llopcdev2detchar="echo $ gsissh detchar@${LLOPCDEV2}; gsissh detchar@${LLOPCDEV2}"
+alias llopcdev1cbc="echo $ gsissh cbc@${LLOPCDEV1}; gsissh cbc@${LLOPCDEV1}"
+alias atlas1cbc="echo $ gsissh cbc@${ATLAS1}; gsissh cbc@${ATLAS1}"
+alias atlas2cbc="echo $ gsissh cbc@${ATLAS2}; gsissh cbc@${ATLAS2}"
+alias atlas3cbc="echo $ gsissh cbc@${ATLAS3}; gsissh cbc@${ATLAS3}"
+alias atlas4cbc="echo $ gsissh cbc@${ATLAS4}; gsissh cbc@${ATLAS4}"
+alias atlas1geodc="echo $ gsissh geodc@${ATLAS1}; gsissh geodc@${ATLAS1}"
+alias atlas2geodc="echo $ gsissh geodc@${ATLAS2}; gsissh geodc@${ATLAS2}"
+alias atlas3geodc="echo $ gsissh geodc@${ATLAS3}; gsissh geodc@${ATLAS3}"
+alias atlas4geodc="echo $ gsissh geodc@${ATLAS4}; gsissh geodc@${ATLAS4}"
+alias titan1geodc="echo $ gsissh geodc@${TITAN1}; gsissh geodc@${TITAN1}"
+alias titan2geodc="echo $ gsissh geodc@${TITAN2}; gsissh geodc@${TITAN2}"
+alias titan3geodc="echo $ gsissh geodc@${TITAN3}; gsissh geodc@${TITAN3}"
+
+# alias for detchar nodes
+export CITSUMMARY="detchar.ligo.caltech.edu"
+alias citsummary="echo $ gsissh ${CITSUMMARY}; gsissh ${CITSUMMARY}"
+export LHOSUMMARY="detchar.ligo-wa.caltech.edu"
+alias lhosummary="echo $ gsissh ${LHOSUMMARY}; gsissh ${LHOSUMMARY}"
+export LLOSUMMARY="detchar.ligo-la.caltech.edu"
+alias llosummary="echo $ gsissh ${LLOSUMMARY}; gsissh ${LLOSUMMARY}"
+
+alias citsummarydetchar="echo $ detchar@${CITSUMMARY}; gsissh detchar@${CITSUMMARY}"
+alias lhosummarydetchar="echo $ detchar@${LHOSUMMARY}; gsissh detchar@${LHOSUMMARY}"
+alias llosummarydetchar="echo $ detchar@${LLOSUMMARY}; gsissh detchar@${LLOSUMMARY}"
+
+# Virgo computing
+export VIRGO1="farmn1.virgo.infn.it"
+alias virgo1="echo $ ssh macleod@${VIRGO1} && ssh macleod@${VIRGO1}"
+export VIRGO2="farmn2.virgo.infn.it"
+alias virgo2="echo $ ssh macleod@${VIRGO2} && ssh macleod@${VIRGO2}"
+
+# gracedb processor
+export EMFOLLOW="emfollow.ligo.caltech.edu"
+alias emfollow="echo $ gsissh gracedb.processor@${EMFOLLOW} && gsissh gracedb.processor@${EMFOLLOW}"
