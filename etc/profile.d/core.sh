7343341883c568650d412356f8fd70b3803d144f
Duncan Macleod <duncan.macleod@ligo.org>
2017-07-13 13:39:28 +0100

    profile.sh: renamed ls -> core

---
+++ etc/profile.d/core.sh
@@ -0,0 +1,13 @@
+#
+# Profile setter for core utils
+#
+# Author: Duncan Macleod <duncan.macleod@ligo.org>
+#
+
+# aliases
+alias rm='rm -i'
+alias view="vim -R"
+
+# environment
+export CLICOLOR=1
+export EDITOR=vim
