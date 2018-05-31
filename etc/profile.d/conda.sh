9fea00ac4c1e76d015e4db7ca92c4b797c8ffb7c
Duncan Macleod <duncan.macleod@ligo.org>
2018-05-31 11:16:09 +0100

    profile.d: improved conda script

--- etc/profile.d/conda.sh
+++ etc/profile.d/conda.sh
@@ -1,6 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 #
 # profile setter for conda
 
-CONDA_PATH=${HOME}/anaconda2
-alias conda="${HOME}/anaconda2/bin/conda"
+###################################
+# Find {ana,mini}conda installation
+# Returns:
+#   the path to the distribution
+#   (parent of 'bin' dir)
+###################################
+find_conda() {
+    # find HOME
+    if [ -z ${HOME+x} ]; then
+        local _home="/home/$(whoami)"
+    else
+        local _home=${HOME}
+    fi
+
+    # prefer python3 over python3 and anaconda over miniconda
+    for _condaname in anaconda3 anaconda2 miniconda3 miniconda2; do
+        if [ -d ${_home}/${_condaname} ]; then
+            echo ${_home}/${_condaname}
+            return 0
+        fi
+    done
+    return 1
+}
+
+
+# find conda installation
+if [ -z ${CONDA_PATH+x} ]; then
+    CONDA_PATH=$(find_conda)
+fi
+
+# add /bin/ to the PATH
+if [ -d ${CONDA_PATH}/bin ]; then
+    export PATH=${PATH}:${CONDA_PATH}/bin
+fi
