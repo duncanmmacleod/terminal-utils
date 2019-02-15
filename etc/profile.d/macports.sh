99f5a98cb4429106cf89ac548bb5b0f8e0b7ecfc
Duncan Macleod <duncan.macleod@ligo.org>
2019-02-15 09:57:26 +0000

    macports.sh: minor updates, new checksum function

--- etc/profile.d/macports.sh
+++ etc/profile.d/macports.sh
@@ -5,7 +5,15 @@
 #
 
 if [[ "${OSTYPE}" == "darwin"* ]] && [ -f /opt/local/bin/port ]; then
-    alias portupdate="sudo port selfupdate && sudo port upgrade outdated"
+    alias portupdate="sudo port selfupdate && port outdated && sudo port upgrade outdated"
     alias portinstall="sudo port -N install"
     alias portclean="sudo port clean --all installed && sudo port -f uninstall inactive"
 fi
+
+
+function portchecksum {
+    local tarball=$1
+    echo "checksums     rmd160 `openssl dgst -rmd160 $tarball | cut -d\  -f2` \\"
+    echo "              sha256 `openssl dgst -sha256 $tarball | cut -d\  -f2` \\"
+    echo "              size   `stat -f%z $tarball`"
+}
