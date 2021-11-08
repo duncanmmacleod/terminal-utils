af55fb611f947cffbac41db601696357e707178a
Duncan Macleod <duncan.macleod@ligo.org>
2021-11-08 15:23:00 +0000

    install.sh: add install script
    to initially configure a system

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Install the terminal-utils
+
+HERE=$(python3 -c "
+from pathlib import Path
+print(Path('${BASH_SOURCE[0]}').absolute().parent)
+")
+
+# link the profile scripts into ~/etc/
+mkdir -p ~/etc/
+for profitem in ${HERE}/etc/*; do
+	if [ ! -e ~/etc/$(basename ${profitem}) ]; then
+		ln -vs ${profitem} ~/etc/
+	fi
+done
+if [ ! -e ~/.condarc ]; then
+	ln -vs ~/etc/condarc ~/.condarc
+fi
