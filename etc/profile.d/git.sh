e89e4fff98b8d228c9f5139c37c9c59ad5691ba1
Duncan Macleod <macleoddm@cardiff.ac.uk>
2024-07-15 11:38:53 +0100

    profile.d: add git profile
    to enable bash-completion for Debian

---
+++ etc/profile.d/git.sh
@@ -0,0 +1,9 @@
+#
+# Profile setter for git
+#
+# Author: Duncan Macleod <macleoddm@cardiff.ac.uk>
+#
+
+if [ -f /usr/share/bash-completion/completions/git ]; then
+  . /usr/share/bash-completion/completions/git
+fi
